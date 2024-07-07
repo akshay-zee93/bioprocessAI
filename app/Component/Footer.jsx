cca5887c42bfcc26a79267a7ec4e78e6e22e2ca2
akshay pandey <akpan9587@gmail.com>
2024-07-07 10:59:08 +0530

    legal links added

--- app/Component/Footer.jsx
+++ app/Component/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className=" w-screen bg-teal-100 lg:px-20 px-6 py-10  font-normal font-opensans text-base text-blue-gray-200 lg:flex-col justify-start  gap-16 inline-flex">
       <div className=" px-4 lg:px-8 lg:flex-col  h-full item-center gap-12 flex">
         <div className="self-stretch justify-start h-full px-4 lg:px-8  items-start gap-16 inline-flex">
-          <div className="grow shrink basis-0  justify-start items-start gap-8 flex flex-col lg:flex-row">
+          <div className="grow shrink basis-0 justify-center  gap-8 flex flex-col lg:flex-row">
             <div className="grow shrink basis-0 flex-col justify-start items-start gap-4 inline-flex">
               <div className="self-stretch text-white text-3xl lg:text-5xl font-dongle ">
                 BioProcessAi
@@ -75,26 +75,57 @@ const Footer = () => {
               <div className="self-stretch flex-col justify-start items-start gap-3 flex">
                 <div className="justify-start items-center gap-2 inline-flex">
                   <div className="justify-center items-center gap-2 flex">
-                    <div className="font-normal ">
-                      <Link href="/about">About</Link>
-                    </div>
+                    <Link className="link link-hover" href="/about">
+                      About
+                    </Link>
                   </div>
                 </div>
                 <div className="justify-start items-center gap-2 inline-flex">
                   <div className="justify-center items-center gap-2 flex">
-                    <Link href="/contact-for-demo" className="  font-normal ">
+                    <Link
+                      href="/contact-for-demo"
+                      className=" link link-hover "
+                    >
                       Request Demo
                     </Link>
                   </div>
                 </div>
                 <div className="justify-start items-center gap-2 inline-flex">
                   <div className="justify-center items-center gap-2 flex">
-                    <div className="  font-normal ">Blog</div>
+                    <div className="  link link-hover ">Blog</div>
                   </div>
                 </div>
                 <div className="justify-start items-center gap-2 inline-flex">
                   <div className="justify-center items-center gap-2 flex">
-                    <div className="  font-normal ">FAQs</div>
+                    <Link href="" className=" link link-hover ">
+                      FAQs
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 flex-col justify-start items-start gap-4 inline-flex">
+              <div className="self-stretch   font-semibold ">Legal</div>
+              <div className="self-stretch h-24 flex-col justify-start items-start gap-3 flex">
+                <div className="justify-start items-center gap-2 inline-flex">
+                  <div className="justify-center items-center gap-2 flex">
+                    <Link href="" className=" link link-hover ">
+                      Terms
+                    </Link>
+                  </div>
+                </div>
+                <div className="justify-start items-center gap-2 inline-flex">
+                  <div className="justify-center items-center gap-2 flex">
+                    <Link href="" className=" link link-hover ">
+                      Privacy
+                    </Link>
+                  </div>
+                </div>
+                <div className="justify-start items-center gap-2 inline-flex">
+                  <div className="justify-center items-center gap-2 flex">
+                    <Link href="" className=" link link-hover ">
+                      Cookies Policy
+                    </Link>
                   </div>
                 </div>
               </div>
